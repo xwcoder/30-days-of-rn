ce0209ee7005dde9b21af057591327cf43065e10
creep <creep.wei@gmail.com>
2018-03-09 16:45:45 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,38 @@
+30-days-of-rn
+==================
+
+RN练手，将[30-days-of-react-native](https://github.com/fangwei716/30-days-of-react-native)升级到RN 0.54。
+
+## Catalog
+
+- [x] day-0: index page
+- [x] day-1: A stopwatch
+- [ ] day-2: A weather app
+- [ ] day-3: twitter
+- [ ] day-4: cocoapods
+- [ ] day-5: find my location
+- [ ] day-6: Spotify
+- [ ] day-7: Moveable Circle
+- [ ] day-8: Swipe Left Menu
+- [ ] day-9: Twitter Parallax View
+- [ ] day-10: Tumblr Menu
+- [ ] day-11: OpenGL
+- [ ] day-12: charts
+- [ ] day-13: tweet
+- [ ] day-14: tinder
+- [ ] day-15: Time picker
+- [ ] day-16: Gesture unlock
+- [ ] day-17: Fuzzy search
+- [ ] day-18: Sortable
+- [ ] day-19: TouchID to unlock
+- [ ] day-20: Single page Reminder
+- [ ] day-21: Multi page Reminder
+- [ ] day-22: Google Now
+- [ ] day-23: Local WebView
+- [ ] day-24: Youtube scrollable tab
+- [ ] day-25: custome in-app browser
+- [ ] day-26: swipe and switch
+- [ ] day-27: iMessage Gradient
+- [ ] day-28: iMessage image picker
+- [ ] day-29: iMessage image picker
+- [ ] day-30: Push Notifications
--- index.js
+++ index.js
@@ -10,11 +10,31 @@ import {
 } from 'react-native'
 import {StackNavigator} from 'react-navigation'
 import Swiper from 'react-native-swiper'
-import Icon from 'react-native-vector-icons/Ionicons'
+import Ionicon from 'react-native-vector-icons/Ionicons'
 import IconFA from 'react-native-vector-icons/FontAwesome'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
 import util from './views/util'
 import Day1 from './views/day1'
+
+const getIconComponent = type => {
+
+  switch (type) {
+    case 'ion':
+      Icon = Ionicon
+      break
+    case 'fa':
+      Icon = IconFA
+      break
+    case 'mci':
+      Icon = MaterialCommunityIcons
+      break
+    default:
+      Icon = Ionicon
+  }
+
+  return Icon
+}
 
 class Home extends Component {
 
@@ -27,36 +47,43 @@ class Home extends Component {
     super(props)
 
     this.state = {
-      days: [{
-        key: 0,
-        title: "A stopwatch",
-        isFA: false,
+      days:[{
+        key:0,
+        title:"A stopwatch",
+        iconType: 'ion',
         icon: "ios-stopwatch",
         size: 48,
         color: "#ff856c",
         hideNav: false,
-        route: 'Day1'
       },{
         key: 1,
-        title:"A weather app",
-        isFA: false,
-        icon: "ios-partly-sunny",
-        size:60,
-        color:"#90bdc1",
+        title: "A weather app",
+        iconType: 'mci',
+        icon: "weather-partlycloudy",
+        size: 48,
+        color: "#90bdc1",
         hideNav: true,
       },{
-        key: 2,
+        key:2,
         title:"twitter",
+        isFA: false,
+        icon: "social-twitter",
+        size:50,
+        color:"#2aa2ef",
+        hideNav: true,
+      },{
+        key:3,
+        title:"cocoapods",
         isFA: true,
         icon: "contao",
         size:50,
         color:"#FF9A05",
         hideNav: false,
       },{
-        key: 4,
-        title: "find my location",
+        key:4,
+        title:"find my location",
         isFA: false,
-        icon: "md-pin",
+        icon: "ios-location",
         size:50,
         color:"#00D204",
         hideNav: false,
@@ -87,8 +114,8 @@ class Home extends Component {
       },{
         key:8,
         title:"Twitter Parallax View",
-        isFA: true,
-        icon: "twitter-square",
+        isFA: false,
+        icon: "social-twitter-outline",
         size:50,
         color:"#2aa2ef",
         hideNav: true,
@@ -96,23 +123,23 @@ class Home extends Component {
         key:9,
         title:"Tumblr Menu",
         isFA: false,
-        icon: "logo-tumblr",
+        icon: "social-tumblr",
         size:50,
         color:"#37465c",
         hideNav: true,
       },{
-         key:10,
-         title:"OpenGL",
-         isFA: false,
-         icon: "md-contrast",
-         size:50,
-         color:"#2F3600",
-         hideNav: false,
-       },{
+        key:10,
+        title:"OpenGL",
+        isFA: false,
+        icon: "contrast",
+        size:50,
+        color:"#2F3600",
+        hideNav: false,
+      },{
         key:11,
         title:"charts",
         isFA: false,
-        icon: "ios-stats",
+        icon: "stats-bars",
         size:50,
         color:"#fd8f9d",
         hideNav: false,
@@ -120,15 +147,15 @@ class Home extends Component {
         key:12,
         title:"tweet",
         isFA: false,
-        icon: "md-chatboxes",
+        icon: "chatbox-working",
         size:50,
         color:"#83709d",
         hideNav: true,
       },{
         key:13,
         title:"tinder",
-        isFA: true,
-        icon: "fire",
+        isFA: false,
+        icon: "fireball",
         size:50,
         color:"#ff6b6b",
         hideNav: true,
@@ -144,7 +171,7 @@ class Home extends Component {
         key:15,
         title:"Gesture unlock",
         isFA: false,
-        icon: "ios-unlock",
+        icon: "unlocked",
         size:50,
         color:"#32A69B",
         hideNav: true,
@@ -152,7 +179,7 @@ class Home extends Component {
         key:16,
         title:"Fuzzy search",
         isFA: false,
-        icon: "md-search",
+        icon: "search",
         size:50,
         color:"#69B32A",
         hideNav: false,
@@ -160,7 +187,7 @@ class Home extends Component {
         key:17,
         title:"Sortable",
         isFA: false,
-        icon: "md-move",
+        icon: "arrow-move",
         size:50,
         color:"#68231A",
         hideNav: true,
@@ -168,7 +195,7 @@ class Home extends Component {
         key:18,
         title:"TouchID to unlock",
         isFA: false,
-        icon: "ios-log-in",
+        icon: "log-in",
         size:50,
         color:"#fdbded",
         hideNav: true,
@@ -183,6 +210,22 @@ class Home extends Component {
       },{
         key:20,
         title:"Multi page Reminder",
+        isFA: false,
+        icon: "ios-paper-outline",
+        size:50,
+        color:"#fe952b",
+        hideNav: true,
+      },{
+        key:21,
+        title:"Google Now",
+        isFA: false,
+        icon: "android-microphone",
+        size:50,
+        color:"#4285f4",
+        hideNav: true,
+      },{
+        key:22,
+        title:"Local WebView",
         isFA: true,
         icon: "safari",
         size:50,
@@ -192,7 +235,7 @@ class Home extends Component {
         key:23,
         title:"Youtube scrollable tab",
         isFA: false,
-        icon: "logo-youtube",
+        icon: "social-youtube",
         size:50,
         color:"#e32524",
         hideNav: true,
@@ -200,7 +243,7 @@ class Home extends Component {
         key:24,
         title:"custome in-app browser",
         isFA: false,
-        icon: "ios-globe",
+        icon: "ios-world",
         size:50,
         color:"#00ab6b",
         hideNav: true,
@@ -208,7 +251,7 @@ class Home extends Component {
         key:25,
         title:"swipe and switch",
         isFA: false,
-        icon: "md-shuffle",
+        icon: "shuffle",
         size:50,
         color:"#893D54",
         hideNav: true,
@@ -216,7 +259,7 @@ class Home extends Component {
         key:26,
         title:"iMessage Gradient",
         isFA: false,
-        icon: "ios-chatbubbles",
+        icon: "ios-chatbubble",
         size:50,
         color:"#248ef5",
         hideNav: false,
@@ -224,15 +267,15 @@ class Home extends Component {
         key:27,
         title:"iMessage image picker",
         isFA: false,
-        icon: "md-images",
+        icon: "images",
         size:50,
         color:"#f5248e",
         hideNav: true,
       },{
         key:28,
-        title:"3d touch",
+        title:"iMessage image picker",
         isFA: false,
-        icon: "ios-navigate",
+        icon: "navicon-round",
         size:50,
         color:"#48f52e",
         hideNav: false,
@@ -240,7 +283,7 @@ class Home extends Component {
         key:29,
         title:"Push Notifications",
         isFA: false,
-        icon: "md-notifications",
+        icon: "android-notifications",
         size:50,
         color:"#f27405",
         hideNav: false,
@@ -258,11 +301,13 @@ class Home extends Component {
     let swiperHeight = util.size.width / ratio
 
     let boxes = this.state.days.map((elem, index) => {
+
+      let Icon = getIconComponent(elem.iconType)
+
       return (
         <TouchableHighlight onPress={() => this.navigate(`Day${index + 1}`)} key={elem.key} style={styles.touchBox}>
           <View style={styles.boxContainer}>
-            {elem.isFA ? <IconFA size={elem.size} name={elem.icon} style={[styles.boxIcon, {color: elem.color}]}/>
-              : <Icon size={elem.size} name={elem.icon} style={[styles.boxIcon, {color: elem.color}]}/>}
+            <Icon size={elem.size} name={elem.icon} style={[styles.boxIcon, {color: elem.color}]}/>
             <Text style={styles.boxText}>Day{index + 1}</Text>
           </View>
         </TouchableHighlight>
